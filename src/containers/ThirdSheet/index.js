b9c47d2821b6957b53c3d62d46039a08eeded85f
Bhaskar Neogi <v-bhaskar.neogi@rebelfoods.com>
2022-04-04 14:02:54 +0530

    calender range

---
+++ src/containers/ThirdSheet/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DatePickerComponent from '../../components/DatePicker'
+
+const ThirdSheetComponent = () => {
+  return (
+    <React.Fragment>
+      <DatePickerComponent />
+    </React.Fragment>
+    )
+}
+
+export default ThirdSheetComponent
