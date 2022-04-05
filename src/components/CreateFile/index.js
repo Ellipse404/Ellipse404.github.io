01f981e3c261bb83c91fb2d5c9a77b3fdaf915c9
Bhaskar Neogi <v-bhaskar.neogi@rebelfoods.com>
2022-04-05 18:43:10 +0530

    empty create file

---
+++ src/components/CreateFile/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const CreateFileComponent = () => {
+  const [data, setData] = useState(null)
+
+// fetch data from API & state store
+
+  return (
+    <React.Fragment>
+      
+    </React.Fragment>
+  )
+}
+
+export default CreateFileComponent
