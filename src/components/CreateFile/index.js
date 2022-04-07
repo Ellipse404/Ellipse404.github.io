3fe6b203a9152253cff5951a3a624ef04cca61f3
Bhaskar Neogi <v-bhaskar.neogi@rebelfoods.com>
2022-04-07 15:12:17 +0530

    btn style

--- src/components/CreateFile/index.js
+++ src/components/CreateFile/index.js
@@ -1,43 +1,59 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { TextField, Box, FormControl, Button } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  downloadBtn: {
+    width: "auto",
+    height: "36px",
+    backgroundColor: "#009999 !important",
+  },
+
+  typo1: {
+    color: "whitesmoke",
+    fontSize: "18px",
+  },
+}));
 
 const CreateFileComponent = () => {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
+  const classes = useStyles();
 
-// fetch data from API & state store
+  // fetch data from API & state store
 
-useEffect(() => {
-  axios(process.env.REACT_APP_API_URL)
-   .then((res) => {
-     setData(res.data)
-   })
-   .catch((error) => {
-     console.log("Error getting the data : ", error)
-   })
-}, [])
+  useEffect(() => {
+    axios(process.env.REACT_APP_API_URL)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => {
+        console.log("Error getting the data : ", error);
+      });
+  }, []);
 
-// create file
-const downloadTxtFile = () => {
-
-  const createFile = document.createElement("a");
-  const file = new Blob([JSON.stringify(data)], {type: 'text/plain'})
-  createFile.href = URL.createObjectURL(file)
-  createFile.download = "dataFile.txt";
-  document.body.appendChild(createFile);
-  createFile.click()
-}
+  // create file
+  const downloadTxtFile = () => {
+    const createFile = document.createElement("a");
+    const file = new Blob([JSON.stringify(data)], { type: "text/plain" });
+    createFile.href = URL.createObjectURL(file);
+    createFile.download = "dataFile.txt";
+    document.body.appendChild(createFile);
+    createFile.click();
+  };
 
   return (
     <React.Fragment>
-      { console.log("API DATA => ", typeof(JSON.stringify(data)))}
-      <Box>
-        <Button onClick={downloadTxtFile}>
-          Download Data
+      {console.log("API DATA => ", typeof JSON.stringify(data))}
+      <Box sx={{ textAlign: "right" }}>
+        <Button className={classes.downloadBtn} onClick={downloadTxtFile}>
+          <Typography className={classes.typo1}>
+            <pre>Download Data</pre>
+          </Typography>
         </Button>
-      </Box>      
+      </Box>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default CreateFileComponent
+export default CreateFileComponent;
