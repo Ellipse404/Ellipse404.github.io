661aec359ee4a80e6f2fc5312497f452f1ac0749
Bhaskar Neogi <v-bhaskar.neogi@rebelfoods.com>
2022-04-13 16:06:44 +0530

    Text to Speech

--- src/components/CkEditor/index.js
+++ src/components/CkEditor/index.js
@@ -1,14 +1,33 @@
-import React from 'react'
-import { CKEditor } from 'ckeditor4-react';
-import TreeViewComponent from '../TreeMap';
+import React, { useState } from "react";
+import { CKEditor } from "ckeditor4-react";
+import TreeViewComponent from "../TreeMap";
+import TextToSpeechComponent from "../Text2Speech";
+import { Box } from "@mui/material";
 
 const ExtraComponent = () => {
+  const [value, setValue] = useState("");
   return (
     <React.Fragment>
-      <CKEditor initData={<p>Try the Editor !!</p>} />
+      <CKEditor
+        onChange={(e, editor) => {
+          const data = e.editor.getData();
+          setValue(data);
+        }}
+      />
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          height: "70px",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <TextToSpeechComponent word={value} btnText="Read this out" />
+      </Box>
       <TreeViewComponent />
     </React.Fragment>
-    )
-}
+  );
+};
 
-export default ExtraComponent
+export default ExtraComponent;
--- src/components/Text2Speech/index.js
+++ src/components/Text2Speech/index.js
@@ -2,12 +2,12 @@ import { Button } from "@mui/material";
 import React from "react";
 import { useSpeechSynthesis } from "react-speech-kit";
 
-const TextToSpeechComponent = () => {
+const TextToSpeechComponent = (props) => {
   const { speak } = useSpeechSynthesis();
   return (
     <React.Fragment>
-      <Button color="primary" variant="contained" onClick={() => speak({text: "Hello Speech Recognition !!"})}>
-        Speak
+      <Button color="primary" variant="contained" onClick={() => speak({text:  props.word || "Hello !!" })}>
+        {props.btnText || "SPEAK"}
       </Button>
     </React.Fragment>
   );
