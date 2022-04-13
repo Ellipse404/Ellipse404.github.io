81808ccdf9ea9e409a58a25ab681b362c580b158
Bhaskar Neogi <v-bhaskar.neogi@rebelfoods.com>
2022-04-13 16:49:30 +0530

    voice tone selection

--- src/components/Text2Speech/index.js
+++ src/components/Text2Speech/index.js
@@ -1,23 +1,64 @@
-import { Button } from "@mui/material";
-import React from "react";
+import {
+  Button,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from "@mui/material";
+import React, { useState } from "react";
 import { useSpeechSynthesis } from "react-speech-kit";
 
 const TextToSpeechComponent = (props) => {
+  const [voiceIndex, setVoiceIndex] = useState(null);
   const onEnd = () => {
     // You could do something here after speaking has finished
   };
   const { speak, cancel, speaking, supported, voices } = useSpeechSynthesis({
     onEnd,
   });
+  const voice = voices[voiceIndex] || null;
+
   return (
     <React.Fragment>
-      { !supported && (
+      {!supported && (
         <pre>Your browser does not support speech recognition !!</pre>
       )}
 
-     { supported &&(<Button color="primary" variant="contained" onClick={() => speak({text:  props.word || "Hello !!" })}>
-        {props.btnText || "SPEAK"}
-      </Button>)}
+      {supported && (
+        <>
+          <FormControl sx={{ m: 1, minWidth: 80 }}>
+            <InputLabel id="demo-simple-select-label">
+              {" "}
+              Choose Voicetone
+            </InputLabel>
+            <Select
+              id="voice"
+              sx={{ width: "450px !important" }}
+              name="voice"
+              value={voiceIndex || ""}
+              onChange={(event) => {
+                setVoiceIndex(event.target.value);
+              }}
+            >
+              <MenuItem value="">Default</MenuItem>
+              {voices.map((option, index) => (
+                <MenuItem key={option.voiceURI} value={index}>
+                  {`${option.lang} - ${option.name}`}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={() =>
+              speak({ text: props.word || "Hey, this is Empty !!", voice })
+            }
+          >
+            {props.btnText || "SPEAK"}
+          </Button>
+        </>
+      )}
     </React.Fragment>
   );
 };
