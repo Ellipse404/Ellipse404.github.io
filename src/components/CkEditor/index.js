08aef4d8043b158711a672587e9aacbd67e3e068
Bhaskar Neogi <v-bhaskar.neogi@rebelfoods.com>
2022-04-13 11:47:36 +0530

    Tree Map View

--- src/components/CkEditor/index.js
+++ src/components/CkEditor/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { CKEditor } from 'ckeditor4-react';
+import TreeViewComponent from '../TreeMap';
 
-const EditorComponent = () => {
+const ExtraComponent = () => {
   return (
     <React.Fragment>
       <CKEditor initData={<p>Try the Editor !!</p>} />
+      <TreeViewComponent />
     </React.Fragment>
     )
 }
 
-export default EditorComponent
+export default ExtraComponent
